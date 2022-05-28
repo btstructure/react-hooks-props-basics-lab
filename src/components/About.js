83ac5113ff30736e2e7330cb553a83eb88136b3d
Beyhan <beyhan.taylan@hotmail.com>
2022-05-28 15:16:50 -0400

    commit tree d215cbd917a7ebafaabf820bbd12a317fb849619

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,38 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
+      
     </div>
   );
 }
+
+// export default About;
+
+// import React from "react";
+// import Links from "./Links";
+
+
+
+// function About(props) {
+//   function ifBio() {
+//     return props.bio ? <p>{props.bio}</p> : null
+//   }
+
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       {ifBio()}
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       <Links github={props.links.github} linkedin={props.links.linkedin} />
+//     </div>
+//   );
+// }
 
 export default About;
